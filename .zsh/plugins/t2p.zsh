c19445524b363f6fb9f87372ea4b52f6978e233c
masami <msmaiplruri@gmail.com>
2014-10-26 23:44:50 +0900

    t2p update

--- .zsh/plugins/t2p.zsh
+++ .zsh/plugins/t2p.zsh
@@ -1,22 +1,40 @@
-function _mkdirs {
-    if [ ! -d log ]; then; mkdir log; fi;
-    if [ ! -d aux ]; then; mkdir aux; fi;
-    if [ ! -d pdf ]; then; mkdir pdf; fi;
-    if [ ! -d dvi ]; then; mkdir dvi; fi;
+#!/bin/zsh
+function _t2p-mkdirs {
+    lst=(log aux pdf dvi)
+    for v in $lst; do
+        if [ ! -d $v ]; then
+            mkdir $v
+            echo "mkdir $v"
+        fi
+    done
 }
 
-function _mvs {
+function _t2p-mvs {
     setopt nonomatch
+    lst=(log aux pdf dvi)
+    for v in $lst; do
+        if ls *.$v > /dev/null 2>&1; then; mv *.$v $v/; fi
+    done
+}
 
-    if ls *.log > /dev/null 2>&1; then; mv *.log log/; fi;
-    if ls *.aux > /dev/null 2>&1; then; mv *.aux aux/; fi;
-    if ls *.pdf > /dev/null 2>&1; then; mv *.pdf pdf/; fi;
-    if ls *.dvi > /dev/null 2>&1; then; mv *.dvi dvi/; fi;
+function _t2p-clean {
+    lst=(log aux dvi)
+    for v in $lst; do
+        fn=$v/$1.$v
+	    if [ -e $fn ]; then
+            rm $fn
+            echo "rm $fn"
+        fi
+    done
 }
 
 function t2p {
-    ptex2pdf -u -l -i $1
-    _mkdirs
-    _mvs
+    if [ $1 = "--clear" -o $1 = "-C" ]; then
+        fn=$(echo $2 | sed -e s/\.tex//)
+        _t2p-clean $fn
+    else
+        ptex2pdf -u -l -i $1
+        _t2p-mkdirs
+        _t2p-mvs
+    fi
 }
-
